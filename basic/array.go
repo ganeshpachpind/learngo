5966e25b838dec8e577518c755973e9573484b19
ganeshpachpind <ganesh.a.pachpind@metrosystems.net>
2018-11-10 01:32:58 +0100

    Improved array example

--- basic/array.go
+++ basic/array.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
-  arr:=[4]int{1,4,6,7}
+func main() {
+	arr := [4]int{1, 4, 6, 7}
 
-  for i:=0 ;i< len(arr); i++ {
-    fmt.Println("val", arr[i])
-  }
+	for i := 0; i < len(arr); i++ {
+		fmt.Println("val", arr[i])
+	}
+
+	data := [5]float64{10, 20, 30, 40, 50.4}
+
+	var res float64
+	for i := 0; i < 5; i++ {
+		res = res + data[i]
+	}
+
+	fmt.Println("Result is ", res)
 }
